204b192c688201b745e21731f07145a6b9608165
Nguyễn Hoài Tâm <nguyenhoaitam16082003@gmail.com>
2022-08-08 11:11:47 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// JS BACK TO TOP
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $('#backtop').fadeIn();
+        }
+        else {
+            $('#backtop').fadeOut();
+        }
+    });
+
+    $("#backtop").click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+});
